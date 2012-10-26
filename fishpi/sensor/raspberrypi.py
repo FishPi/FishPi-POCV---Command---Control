740a4b634fe565f1c7410c41cce625a7f3dd6303
grayal <al.p.gray@gmail.com>
2012-10-26 22:45:15 +0100

    added check for i2c bus according to pi board revision

---
+++ fishpi/sensor/raspberrypi.py
@@ -0,0 +1,26 @@
+
+#
+# FishPi - An autonomous drop in the ocean
+#
+# Simple helper for the Raspberry Pi
+#
+
+import subprocess
+
+def board_ver():
+	proc = subprocess.Popen(['cat', '/proc/cpuinfo'], stdout=subprocess.PIPE, close_fds=True)
+	out, err = proc.communicate()
+	lines = filter(None, out.split("\n"))
+	results = {}
+	for line in lines:
+		k, v = line.strip().split(':')
+		results[k.strip()] = v.strip()
+	return results['Revision']
+
+def i2c_bus():
+	ver = board_ver()
+	if int(ver) > 4:
+		return 1
+	else:
+		return 0
+
