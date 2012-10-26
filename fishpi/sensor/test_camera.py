a382801bf64d95cd791521510c06b557158650c5
grayal <al.p.gray@gmail.com>
2012-10-26 23:45:13 +0100

    split camera test files

--- fishpi/sensor/test_camera.py
+++ fishpi/sensor/test_camera.py
@@ -7,7 +7,6 @@
 #
 
 from camera import SingleCamera
-from camera import StereoCamera
 
 if __name__ == "__main__":
     print "testing single camera..."
@@ -33,24 +32,3 @@ if __name__ == "__main__":
     print "click.."
     cam = "done"
 
-    print "testing stereo camera..."
-    cam = StereoCamera("/dev/video0", "/dev/video1", (320,240))
-    print "click.."
-    cam.capture("/home/pi/fishpi/imgs")
-    print "click.."
-    cam.capture("/home/pi/fishpi/imgs")
-    print "click.."
-    cam.capture("/home/pi/fishpi/imgs")
-    cam = "done"
-
-    print "and in YUV..."
-    cam = StereoCamera("/dev/video0", "/dev/video1", (320,240), "YUV")
-    print "click.."
-    cam.capture("/home/pi/fishpi/imgs")
-    cam = "done"
-    
-    print "and in HSV..."
-    cam = StereoCamera("/dev/video0", "/dev/video1", (320,240), "HSV")
-    print "click.."
-    cam.capture("/home/pi/fishpi/imgs")
-    cam = "done"
