6d6a2c9c7ff6c9351e05f74d1cad5ea53ba7fa62
grayal <al.p.gray@gmail.com>
2012-11-20 18:20:01 +0000

    split drive controller to allow other pwm interface eg raspy juice

--- fishpi/vehicle/test_raspy.py
+++ fishpi/vehicle/test_raspy.py
@@ -9,6 +9,7 @@
 import raspberrypi
 
 from time import sleep
+from drive_controller import PyJuiceDriveController
 
 if __name__ == "__main__":
     
@@ -29,3 +30,65 @@ if __name__ == "__main__":
     print "centre"
     i2c_bus.write_word_data(addr, servo_number, 1500)
     
+    print "testing drive controller..."
+    drive = PyJuiceDriveController(debug=True, i2c_bus)
+    
+    print "run full ahead for 5 sec..."
+    drive.set_throttle(1.0)
+    sleep(5)
+    
+    print "run 50% ahead for 5 sec..."
+    drive.set_throttle(0.5)
+    sleep(5)
+    
+    print "run 0% for 5 sec..."
+    drive.set_throttle(0.0)
+    sleep(5)
+    
+    print "run 50% reverse for 5 sec"
+    drive.set_throttle(-0.5)
+    sleep(5)
+    
+    print "run full reverse for 5 sec"
+    drive.set_throttle(-1.0)
+    sleep(5)
+    
+    print "and back to neutral..."
+    drive.set_throttle(0.0)
+    sleep(5)
+    
+    print "check out of bounds errors"
+    try:
+        drive.set_throttle(15.0)
+    except ValueError:
+        print "caught 15"
+    
+    try:
+        drive.set_throttle(-10.0)
+    except ValueError:
+        print "caught -10"
+    
+    # test steering
+    print "steer hard to port for 5 sec"
+    drive.set_heading(-0.785398)
+    sleep(5)
+    
+    print "steer to port for 5 sec"
+    drive.set_heading(-0.3927)
+    sleep(5)
+    
+    print "and back to neutral..."
+    drive.set_heading(0.0)
+    sleep(5)
+    
+    print "steer to starboard for 5 sec"
+    drive.set_heading(0.3927)
+    sleep(5)
+    
+    print "steer hard to starboard for 5 sec"
+    drive.set_heading(0.785398)
+    sleep(5)
+    
+    print "and back to neutral..."
+    drive.set_heading(0.0)
+    sleep(5)
