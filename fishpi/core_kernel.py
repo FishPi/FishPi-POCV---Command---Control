d5b04adaf1d39b65967bb6f4976eb218a76f58ab
grayal <al.p.gray@gmail.com>
2012-11-22 12:53:20 +0000

    tidied logging

--- fishpi/core_kernel.py
+++ fishpi/core_kernel.py
@@ -53,46 +53,46 @@ class FishPiKernel:
             self.read_time()
         except Exception as ex:
             self.data.has_time = False
-            logging.info('CORE:\tError in update loop (TIME) - %s' % ex)
+            logging.info("CORE:\tError in update loop (TIME) - %s" % ex)
                 
         try:
             self.read_GPS()
         except Exception as ex:
             self.data.has_GPS = False
-            logging.info('CORE:\tError in update loop (GPS) - %s' % ex)
+            logging.info("CORE:\tError in update loop (GPS) - %s" % ex)
 
         try:
             self.read_compass()
         except Exception as ex:
             self.data.has_compass = False
-            logging.info('CORE:\tError in update loop (COMPASS) - %s' % ex)
+            logging.info("CORE:\tError in update loop (COMPASS) - %s" % ex)
 
         try:
             self.read_temperature()
         except Exception as ex:
             self.data.has_temperature = False
-            logging.info('CORE:\tError in update loop (TEMPERATURE) - %s' % ex)
+            logging.info("CORE:\tError in update loop (TEMPERATURE) - %s" % ex)
         
         try:
             self.capture_img()
         except Exception as ex:
-            logging.info('CORE:\tError in update loop (CAMERA) - %s' % ex)
+            logging.info("CORE:\tError in update loop (CAMERA) - %s" % ex)
 
         try:
             self._perception_unit.update(self.data)
         except Exception as ex:
-            logging.info('CORE:\tError in update loop (PERCEPTION) - %s' % ex)
+            logging.info("CORE:\tError in update loop (PERCEPTION) - %s" % ex)
         
         try:
             self._navigation_unit.update()
         except Exception as ex:
-            logging.info('CORE:\tError in update loop (NAVIGATION) - %s' % ex)
+            logging.info("CORE:\tError in update loop (NAVIGATION) - %s" % ex)
         
             
     # Devices
     
     def list_devices(self):
-        logging.info("Listing devices...")
+        logging.info("CORE:\tListing devices...")
         for device in self.config.devices:
             logging.info(device)
     
@@ -189,5 +189,5 @@ class FishPiKernel:
         self._drive_controller.halt()
 
     def load_gpx(self, filename):
-        gpx = self.perception_unit.load_gpx(filename)
+        gpx = self._perception_unit.load_gpx(filename)
         return gpx
