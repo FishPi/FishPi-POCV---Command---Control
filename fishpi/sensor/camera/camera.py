9f7b638b00714471545be77e4ccb5b117c6ad10c
Sven <sven.chmie@googlemail.com>
2014-06-10 22:35:18 -0300

    Further implementation of the camera library. Refactoring and beautification of various files.

--- fishpi/sensor/camera/camera.py
+++ fishpi/sensor/camera/camera.py
@@ -20,82 +20,61 @@
 from __future__ import with_statement
 import io
 import logging
-import picamera
 import socket
 import struct
 import time
 import threading
 
 
-class CameraConfigError(Exception):
-    pass
-
-
 class CameraController(object):
     _cam_thread = None
-
+    modes = ['stop', 'single', 'continuous', 'video', 'demo']
 
     def __init__(self, interface="", hw_interface=None, debug=False, server='0.0.0.0', port='8000', initial_mode=None):
         self.server = server
         self.port = int(port)
         self.debug = debug
         self.configure(initial_mode)
+        self.enabled = True  # what's this for? The Kernel wants it, but why?
 
-    def set_mode(self, camera_cmd):
-        """ External command interface. More commands to be added later """
-        logging.info("CAM:\tReceived command %s" % camera_cmd)
-        if camera_cmd == "start_image_capture":
-            self.start_image_capture()
-        elif camera_cmd == "stop_image_capture":
-            self.stop_image_capture()
+    def set_up(self, initial_mode):
+        """ Create and start the camera thread """
+        logging.info("CAM:\tSetting up PiCamera")
 
-    def configure(self, initial_mode):
-        logging.info("CAM:\tConfiguring PiCamera")
-
-        # self.lock = threading.Lock()
         self._cam_thread = CameraThread(self.server, self.port)
         self._cam_thread.daemon = True
-        # self._cam_thread.start()
+        self._cam_thread.start()
         if initial_mode is not None:
             self.set_mode(initial_mode)
 
-    def clean_up(self):
+    def tear_down(self):
+        """ Stop and destroy the camera thread """
         logging.info("CAM:\tCleaning up")
         if self._cam_thread is None:
             logging.error("CAM:\tNot configured correctly")
             return
-        self._cam_thread.stop = True
+        self._cam_thread.mode = 'stop'  # deactivate capturing
+        self._cam_thread.stop = True    # stop thread
         self._cam_thread.join()
+
+    def set_mode(self, camera_cmd):
+        """ External command interface. More commands to be added later """
+        logging.info("CAM:\tReceived command %s" % camera_cmd)
+        # If the thread is dead, restart it
+        if self._cam_thread is None or not self._cam_thread.is_alive():
+            logging.error("CAM:\tCamera thread is dead. Restarting..")
+            self.tear_down()
+            self.set_up('stop')
+        if camera_cmd not in self.modes:
+            logging.error("CAM:\tIllegal mode %s received. Ignoring." %
+                camera_cmd)
+            return
+        self._cam_thread.mode = camera_cmd
 
     def capture_now(self):
         """ Captures a picture with the current configuration
             and sends it out """
-
-    def start_video_capture(self):
-        """ Starts the video capture with the current configuration """
         pass
-
-    def stop_video_capture(self):
-        """ Stops a running video capture """
-        pass
-
-    def start_image_capture(self):
-        """ Starts continous image capture with the current configuration """
-        logging.info("CAM:\tStarting image capture")
-        if self._cam_thread is None:
-            logging.error("CAM:\tNot configured correctly")
-            return
-        if not self._cam_thread.is_alive():
-            self._cam_thread.stop = False
-            self._cam_thread.start()
-
-    def stop_image_capture(self):
-        """ Stops a running continous image captureing """
-        logging.info("CAM:\tStopping image capture")
-        if self._cam_thread is None:
-            logging.error("CAM:\tNot configured correctly")
-            return
-        self._cam_thread.stop = True
 
 
 class CameraThread(threading.Thread):
@@ -104,9 +83,26 @@ class CameraThread(threading.Thread):
         threading.Thread.__init__(self)
         self.ip = server_address
         self.port = server_port
-        self.stop = True
+        self.stop = False
+        self.demo_lock = False  # set to true to lock mode variable
+        self.mode = 'stop'
 
     def run(self):
+        """ The thread's run method. There are three possible modes:
+            single capture: The thread sleeps and waits for a command to take a
+                            single picture.
+            continous capture: The thread continously takes pictures.
+            video capture: The thread captures a video stream.
+            A fourth mode could be sending a test picture. """
+
+        # try to import picamera, if it fails, lock to demo mode
+        try:
+            import picamera
+        except ImportError:
+            logging.error("CAM:\tFailed to import picamera module. " +
+                "Running demo mode..")
+            self.demo_lock = True
+
         logging.info("CAM:\tCamera thread running")
         server_socket = socket.socket()
         server_socket.bind((self.ip, self.port))
@@ -114,32 +110,72 @@ class CameraThread(threading.Thread):
         # Accept a single connection and make a file-like object out of it
         connection = server_socket.accept()[0].makefile('rb')
 
-        while self.stop:
-            time.sleep(1)
-
         try:
-            with picamera.PiCamera() as camera:
-                camera.resolution = (640, 480)
-                # Start a preview and let the camera warm up for 2 seconds
-                camera.start_preview()
-                time.sleep(2)
-                stream = io.BytesIO()
-                for foo in camera.capture_continuous(stream, 'jpeg'):
-                    # Write the length of the capture to the stream and flush to
-                    # ensure it actually gets sent
-                    connection.write(struct.pack('<L', stream.tell()))
-                    connection.flush()
-                    # Rewind the stream and send the image data over the wire
-                    stream.seek(0)
-                    connection.write(stream.read())
-                    # When stop is set, get out.
-                    if self.stop:
-                        break
-                    # Reset the stream for the next capture
-                    stream.seek(0)
-                    stream.truncate()
-            # Write a length of zero to the stream to signal we're done
-            connection.write(struct.pack('<L', 0))
+            while not self.stop:
+                if self.mode == 'stop':
+                    time.sleep(1)
+                elif self.demo_lock:
+                    self.mode = 'demo'
+                    self._demo_mode(connection)
+                elif self.mode == 'continous':
+                    self._capture_continuous(connection)
+                elif self.mode == 'video':
+                    self._capture_video(connection)
+                elif self.mode == 'single':
+                    self._capture_single(connection)
+                else:
+                    pass
+                    # Here be dragons
         finally:
             connection.close()
             server_socket.close()
+
+    def _capture_continuous(self, connection):
+        """ Continously capture pictures and send them """
+        logging.info("CAM:\tStarting image capture")
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640, 480)
+            # Start a preview and let the camera warm up for 2 seconds
+            camera.start_preview()
+            time.sleep(2)
+            stream = io.BytesIO()
+            for foo in camera.capture_continuous(stream, 'jpeg'):
+                # When mode is not continuous, get out.
+                if self.mode != 'continuous':
+                    break
+                # Write the length of the capture to the stream and flush to
+                # ensure it actually gets sent
+                connection.write(struct.pack('<L', stream.tell()))
+                connection.flush()
+                # Rewind the stream and send the image data over the wire
+                stream.seek(0)
+                connection.write(stream.read())
+                # Reset the stream for the next capture
+                stream.seek(0)
+                stream.truncate()
+            # Write a length of zero to the stream to signal we're done
+            connection.write(struct.pack('<L', 0))
+        logging.info("CAM:\tStopping image capture")
+
+    def _capture_single(self, connection):
+        """ Take a single picture and send it.
+            Can take up to a second to respond """
+        pass
+
+    def _capture_video(self, connection):
+        """ Capture a video stream and send it """
+        pass
+
+    def _demo_mode(self, connection):
+        """ Continuously broadcast a demo image """
+        stream = io.open('test.bmp', 'rb')
+        stream.seek(0, 2)
+
+        while self.mode == 'demo':
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            stream.seek(0)
+            connection.write(stream.read())
+            stream.seek(0, 2)
+        # Write a length of zero to the stream to signal that we're done
+        connection.write(struct.pack('<L', 0))
--- fishpi/web/webhost.py
+++ fishpi/web/webhost.py
@@ -6,9 +6,9 @@
 #
 
 import logging
-import math
+# import math
 
-from twisted.internet.protocol import Factory, Protocol
+from twisted.internet.protocol import Factory  # , Protocol
 from twisted.internet import reactor
 from twisted.protocols.amp import AMP
 
@@ -17,56 +17,61 @@ from commands import *
 # callback interval in seconds
 callback_interval = 1
 
+
 def run_main_host(kernel, rpc_port):
     """ Runs main RPC server. """
 
     # heartbeat gets signaled off at each update and on at each client call
     heartbeat = Heartbeat()
-    
+
     reactor.listenTCP(rpc_port, RPCHostFactory(kernel, heartbeat))
     logging.info("RPC:\tRPC server started on port %s...", rpc_port)
     reactor.callLater(5, update_callback, kernel, heartbeat)
     reactor.run()
 
+
 def update_callback(kernel, heartbeat):
-    """ Callback to perform updates etc. Needs to reregister callback at end. """
+    """ Callback to perform updates etc.
+        Needs to reregister callback at end. """
     # update kernel
     logging.debug("RPC:\tIn update...")
-    
+
     # check client pulse - otherwise halt vehicle
     if not(heartbeat.is_alive()):
         logging.debug("RPC:\tNo client keep alive call - halting.")
         kernel.halt()
-    
+
     # do update
     kernel.update()
-    
+
     # expect client pulse before next update
     heartbeat.reset()
     # reregister callback
     reactor.callLater(callback_interval, update_callback, kernel, heartbeat)
 
+
 class RPCHostFactory(Factory):
-    
+
     def __init__(self, kernel, heartbeat):
         self.core_kernel = kernel
         self.heartbeat = heartbeat
         self.protocol = RPCHost
         self.clients = []
 
+
 class RPCHost(AMP):
     """ Main RPC host. """
-    
+
     def __init__(self):
         logging.debug("RPC:\tprotocol created.")
-    
+
     def connectionMade(self):
         logging.debug("RPC:\tClient connected.")
         self.factory.clients.append(self)
         self._kernel = self.factory.core_kernel
         self._heartbeat = self.factory.heartbeat
         self._heartbeat.pulse()
-    
+
     def connectionLost(self, reason):
         logging.debug("RPC:\tClient disconnected.")
         self.factory.clients.remove(self)
@@ -75,17 +80,18 @@ class RPCHost(AMP):
 
     @HeartbeatCmd.responder
     def heartbeat_cmd(self, enabled):
-        """ Enable or disable keep alive signal (or just pulse if no content). """
+        """ Enable or disable keep alive signal
+            (or just pulse if no content). """
         self._heartbeat.pulse()
         self._heartbeat.enabled = enabled
-        return {'status':enabled}
-    
+        return {'status': enabled}
+
     @HaltCmd.responder
     def halt_cmd(self):
         """ Halt. """
         self._heartbeat.pulse()
         self._kernel.halt()
-        return {'status':True}
+        return {'status': True}
 
     @ModeCmd.responder
     def mode_cmd(self, mode):
@@ -95,8 +101,8 @@ class RPCHost(AMP):
             self._kernel.set_manual_mode()
         elif mode == "auto":
             self._kernel.set_auto_pilot_mode()
-        return {'status':mode}
-    
+        return {'status': mode}
+
     @QueryStatus.responder
     def get_status(self):
         """ Get status. """
@@ -155,32 +161,34 @@ class RPCHost(AMP):
         self.halt_cmd()
         reactor.stop()
 
+
 class Heartbeat:
-    """ Maintains a heartbeat from client to server calls. Could be done on a timeout. """
-    
+    """ Maintains a heartbeat from client to server calls.
+        Could be done on a timeout. """
+
     def __init__(self):
         self._pulse = True
         self._enabled = True
-    
+
     @property
     def enabled(self):
         return self._enabled
-    
+
     @enabled.setter
     def enabled(self, value):
         self._pulse = True
         self._enabled = value
-    
+
     def is_alive(self):
         if self.enabled:
             return self._pulse
         else:
             return True
-    
+
     def pulse(self):
         if self.enabled:
             self._pulse = True
-    
+
     def reset(self):
         if self.enabled:
             self._pulse = False
